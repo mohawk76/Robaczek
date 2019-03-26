b6ef928398588b54deb9c0f11e425ee6638ec06c
mohawk76 <mohawk76@wp.pl>
2019-03-26 19:34:04 +0100

    Start

---
+++ robaczek.py
@@ -0,0 +1,24 @@
+#TODO Stworzyć klasę Obiekt
+#Klasa Obiekt ma atrybuty x i y , które są współrzędnymi obiektu
+
+#TODO Stworzyć klasę Robaczek
+#Klasa Robaczek ma posiadać atrybuty zawierające: prędkość, poziom i najedzenie
+#Ma posiadać metody zjedzOwoc przyjmuje obiekt typu Owoc(dodanie wartości z owocu do parametru najedzenie i usunięcie elementu z listy) oraz metodę sprawdź poziom, która ustawia poziom Robaczka jeżeli zje odpowiednią ilość owoców
+
+#TODO Stworzyć klasę Owoc
+#Klasa Owoc posiada atrybut jedzenie o losowej wartości z przedziału 1-5
+
+#TODO Stworzyć funkcję sprawdzKolizje
+#Przyjmuje 2 argumenty: typu Robaczek oraz Owoc i ma sprawdzać czy znajdują się na tym samym polu
+
+#TODO Stworzyć funkcję wczytajDane
+#Dane na start wczytujemy z pliku(współrzędne obiektów)
+
+#TODO Stworzyć funkcję pobierzWejscie
+#Użytkownik wpisuje na klawiaturze gdzie ma poruszać się robaczek
+
+#TODO Stworzyć funkcję rysujPlansze
+#Rysuje planszę, robaczka i owoce
+
+#TODO Stworzyć funkcję Gra
+#Zawiera główną pętlę gry, która kończy swoje działanie gdy robaczek zje wszystkie owoce(lista owoców będzie pusta)
